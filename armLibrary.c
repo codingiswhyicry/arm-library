326dcd4f17959de5befd3e656715c2505feec517
Amanda Southworth <amanda.southworth@myds.us>
2017-02-26 10:20:32 -0800

    added source code to the .c file

--- armLibrary.c
+++ armLibrary.c
@@ -3,34 +3,35 @@
 //this command spins the base of the arm at a speed for a specified amount of time 
 void spinBase(int speed, int port, int time) {
     
-    
+    motor(int port, int speed); 
+    msleep(int time);
     
 }
 
 //this command moves the mid arm to a specified position
 void move_mid_arm(int position, int port) {
-    
-    
+
+    set_servo_position(int port, int position);
     
 }
 
 //this command moves the claw to a position specified 
 void move_claw(int position, int port) {
     
-    
-    
+    set_servo_position(int port, int position);
+
 }
 
 //this command opens the claw
 void open_claw(int port) {
     
-    
-    
+    set_servo_position(int port, );
+    //add open servo position
 }
 
 //this command closes the claw
 void close_claw(int port) {
-    
-    
-    
+
+    set_servo_position(int port, );
+    //add closing servo position 
 }
--- armLibrary.h
+++ armLibrary.h
@@ -3,10 +3,13 @@
 #define ARM_LIBRARY_H
 
 void spinBase(int speed, int port, int time);
+//time is in milliseconds
 
 void move_mid_arm(int position, int port);
+//position refers to servo position 
 
 void move_claw(int position, int port);
+//position refers to servo position 
 
 void open_claw(int port); 
 
